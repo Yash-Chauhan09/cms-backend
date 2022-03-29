fccc225e1f8285387cf2fdbe7758c55e05aecd60
Yash-Chauhan09 <yash848210@gmail.com>
2022-03-29 12:57:10 +0530

    CMS phase 3

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://freecoedu-cms.herokuapp.com",
+  allowedHeaders: "*",
+}));
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
